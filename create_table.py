1598cc255e731ecd27e4667901b78ff5790e8d32
GreenKuaiKuai <gigichen34@gmail.com>
2021-03-30 18:26:36 +0000

    改歌名當key

--- create_table.py
+++ create_table.py
@@ -11,9 +11,9 @@ n = ['j','mg','n','ng','nr','nrfg','nrt','ns','nt','nz','s','tg']
 ## 反向索引詞表 token:歌曲編號:出現次數
 inverted_index = dict() 
 ## 歌曲編號表  index即是此歌曲編號
-song_index = [] 
-## 文章長度列表
-song_length = []
+# song_index = [] 
+## 歌詞長度列表
+song_length = dict()
 ## 全部文章長度
 all_length = 0
 
@@ -22,8 +22,9 @@ fileList = os.listdir(lyricsPath)
 for lyricsFile in fileList:
 
     ## 儲存歌名
-    song_index.append(lyricsFile[:-4])
-    now_index = len(song_index) - 1
+    # song_index.append(lyricsFile[:-4])
+    song_name = lyricsFile[:-4]
+    # now_index = len(song_index) - 1
     length = 0
 
     print(lyricsFile[:-4]) #去掉副檔名
@@ -40,27 +41,28 @@ for lyricsFile in fileList:
                 if (tokens[num][1] in n and len(tokens[num][0]) > 1): ## 是名詞且大於一個字
                     token = tokens[num][0]
                     if token not in inverted_index:
-                        inverted_index[token] = {now_index: 1}
+                        inverted_index[token] = {song_name: 1}
                     else:
-                        if now_index not in inverted_index[token]:
-                            inverted_index[token][now_index] = 1
+                        if song_name not in inverted_index[token]:
+                            inverted_index[token][song_name] = 1
                         else:
-                            inverted_index[token][now_index] += 1
-    
+                            inverted_index[token][song_name] += 1
+
     ## 儲存長度
-    song_length.append(length)
+    song_length[song_name] = length
     all_length += length
+    
 
 print("全部文章長度: "+ str(all_length) )
-# print(song_index)
+# print(inverted_index)
 # print(song_length)
 
 ## 儲存反向索引表
 with open('inverted_index.json', 'w') as outfile:  
     json.dump(inverted_index, outfile)
 
-with open('song_index.json', 'w') as outfile:  
-    json.dump(song_index, outfile)
+# with open('song_index.json', 'w') as outfile:  
+#     json.dump(song_index, outfile)
 
 with open('song_length.json', 'w') as outfile:  
     json.dump(song_length, outfile)
